7b00c327beb502b32e887df113cc28a0b565d2b4
wqxiong <wqxiong@microport.com>
2021-03-08 13:59:48 +0800

    完成文件切片存储，完成文件切片合并，删除切片

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,30 +1,71 @@
 'use strict';
 
-const  fs = require('fs')
+const fs = require('fs')
 const path = require('path')
+
+const fileDist = path.join('./', 'fileDist');
 
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-    const { ctx } = this;
-    ctx.body = 'hi, egg';
-  }
+	async index() {
+		const { ctx } = this;
+		ctx.body = 'hi, egg';
+	}
 
-  async upload() {
-    const { ctx } = this;
+	async upload() {
+		const { ctx } = this;
+		const { fileName, fileMd5, chunkNth } = ctx.request.body;
+		let file = ctx.request.files[0];
 
-    let file = ctx.request.files[0];
-    console.log(JSON.stringify(file))
-    // let realFile = fs.readFileSync(file.filepath)
+		console.log(JSON.stringify(file));
 
-    // fs.writeFileSync(path.join('./', `uploadfile/${file.filename}`), realFile)
-    // ctx.cleanupRequestFiles()
+		let realFile = fs.readFileSync(file.filepath);
 
-   
-    ctx.body = { code: 200, message: '', data: file.filename}
 
-  }
+		const chunkPath = `uploadfile/${fileName}-${fileMd5}`;
+
+		if (!fs.existsSync(chunkPath)) fs.mkdirSync(chunkPath);
+
+		fs.writeFileSync(path.join('./', `uploadfile/${fileName}-${fileMd5}/${fileName}-${fileMd5}-${chunkNth}`), realFile)
+		// ctx.cleanupRequestFiles()
+
+
+		ctx.body = { code: 200, message: '', data: fileName }
+
+	}
+
+	async mergeChunks() {
+		const { ctx } = this;
+
+		const { fileName, fileMd5, chunksTotal } = ctx.request.body;
+
+		const filePath = path.join(fileDist, fileName);
+		// 创建存储文件
+		fs.writeFileSync(filePath, '');
+
+		const chunksPath = `uploadfile/${fileName}-${fileMd5}`;
+
+		// 获取暂存的所有chunk
+		const chunks = fs.readdirSync(chunksPath);
+
+		if (chunks.length != chunksTotal) {
+			ctx.helper.__errorHandle('文件切片数量不符');
+			return;
+		}
+
+		for (let i = 0; i < chunksTotal; i++) {
+			let tempChunkPath = `${chunksPath}/${fileName}-${fileMd5}-${i}`;
+
+			fs.appendFileSync(filePath, fs.readFileSync(tempChunkPath));
+
+			fs.unlinkSync(tempChunkPath);
+		}
+
+		fs.rmdirSync(chunksPath);
+
+		ctx.body = ctx.helper.__newMsg(200, 'ok');
+	}
 }
 
 module.exports = HomeController;
